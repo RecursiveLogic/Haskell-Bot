bb0e4450a0b629c31105d996327e2e443584fe81
Arcaelyx <qrx9823@gmail.com>
2017-06-21 23:43:44 -0400

    Changed style of code a bit

--- src/Lib.hs
+++ src/Lib.hs
@@ -7,6 +7,7 @@ module Lib ( startApp ) where
 import Control.Concurrent (MVar, newMVar, modifyMVar_, modifyMVar, readMVar)
 import Control.Exception (finally)
 import Control.Monad (forM_, forever)
+import Control.Monad.IO.Class (liftIO)
 import Data.Aeson
 import Data.Aeson.TH
 import Data.Char (isPunctuation, isSpace)
@@ -14,24 +15,24 @@ import Data.Monoid (mappend)
 import Data.Text (Text)
 import Network.Wai
 import Network.Wai.Handler.Warp
+import Network.WebSockets
 import Servant
-
-import qualified Network.WebSockets as WS
 
 baseURL = "https://discordapp.com/api"
 
-type Client = (Text, WS.Connection)
+type Client = (Text, Connection)
 type ServerState = [Client]
 
 newServerState :: ServerState
 newServerState = []
 
-application :: MVar ServerState -> WS.ServerApp
+application :: MVar ServerState -> ServerApp
 application state pending = do
-  conn <- WS.acceptRequest pending
-  WS.forkPingThread conn 30
+  conn <- acceptRequest pending
+  forkPingThread conn 30
 
 startApp :: IO ()
 startApp = do
   state <- newMVar newServerState
-  WS.runServer "127.0.0.1" 9160 $ application state
+  runServer "127.0.0.1" 9160 $ application state
+  liftIO $ putStrLn "Websocket server running on port 9160"
